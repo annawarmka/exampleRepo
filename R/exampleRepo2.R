f4a36a701f41bac895416aa2f1d69b8a47979bad
annawarmka <85186733+annawarmka@users.noreply.github.com>
2021-06-21 13:01:52 -0500

    Updating package

---
+++ R/exampleRepo2.R
@@ -0,0 +1,12 @@
+#' exampleRepo2
+#'
+#' This is va package decription
+#' @author Anna Warmka
+#' \itemize{
+#' \item Github: https://github.com/annawarmka/exampleRepo
+#' \item Webpage: https://annawarmka.github.io/exampleRepo/}
+#'
+#' @docType package
+#' @name exampleRepo2
+NULL
+
---
+++ inst/extdata/saveDataFiles.R
@@ -0,0 +1,19 @@
+# Converting raw data into package data
+library(usethis)
+library(data.table)
+library(magrittr)
+
+rawDataFolder = "C:/exampleRepo/inst/extdata/"
+
+# exampleDataCSV
+exampleDataCSV = data.table::fread(paste0(rawDataFolder,"exampleData.csv"),
+                                   header=T)%>%
+  as.data.frame()
+exampleDataCSV
+use_data(exampleDataCSV, overwrite=T)
+
+# exampleDataR
+exampleDataR = data.frame(sector=c("a","b",'c'),
+                          coefficients=c(1,2,3))
+exampleDataR
+use_data(exampleDataR, overwrite=T)
